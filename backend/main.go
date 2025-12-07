63ae331e5adc01abed6be91c8de92bfd280ab1d4
Your Name <huzaima.amir.8c@gmail.com>
2025-12-07 16:31:01 +0300

    initialize basic entities to add in db

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-  "gorm.io/driver/postgres"
-  "gorm.io/gorm"
-  "log"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+// connection
 func connectToPostgreSQL() (*gorm.DB, error) {  
     dsn := "user=postgres password=ghq92DAU712.9dn dbname=todolister host=localhost port=5432 sslmode=disable"
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -16,6 +19,36 @@ func connectToPostgreSQL() (*gorm.DB, error) {
     return db, nil
 }
 
+
+//  entities:
+type Task struct {
+  //starting time
+  title string
+  description string
+  // deadline time
+  started bool
+  finished bool
+  checklist [] string
+ }
+
+type Event struct {
+  title string
+  description string
+  startingTime time.Time
+  endTime time.Time
+  place string
+  checklist[] string
+}
+
+type Tag struct { // to describe events and tasks?
+  title string
+  description string 
+}
+
+type checklist struct {
+  subtasks []string
+  totalsubtasks, finishedsubtasks int
+}
 
 func main() {
     db, err := connectToPostgreSQL()
